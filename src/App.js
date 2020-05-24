2b323fc91a4a6d41b4fd35133226e4ff7c2689fa
Fabio Martin <fabiomartin2006@gmail.com>
2020-05-24 23:18:49 +0100

    first commmit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+import HomePage  from '../src/homepage.component'
+
+function App() {
+  return (
+      <div>
+         <HomePage /> 
+      </div>
+  )
+    
+  
+  
+}
+
+export default App;
