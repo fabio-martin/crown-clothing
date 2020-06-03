7fd740e900e98e4b7e63fc9b56bda1b681dce316
Fabio Martin <fabiomartin2006@gmail.com>
2020-06-03 23:33:13 +0100

    updating our app to redirect to home if user is signed in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -63,7 +63,7 @@ class SignIn extends React.Component {
                     />
                   
                     <div className='buttons'>
-                        <CustomButton type='button'>Sign In</CustomButton>
+                        <CustomButton type='submit'>Sign In</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                             Sign In with Google 
                         </CustomButton>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,5 @@
 import { createStore, applyMiddleware} from 'redux'
-
-
 import logger from 'redux-logger'
-
 import rootReducer from '../redux/root-reducer'
 
 
